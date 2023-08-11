b16938ddc323e081cf9fc2a6ae75601c4365cbbc
josue rodriguez blanco <josuebrandon99@gmail.com>
2023-08-11 09:28:17 -0400

    subiendo logica

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useState,useEffect} from "react";
 import { BsMic } from "react-icons/bs";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import "./styles.css";
 
 const Footer = () => {
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
   const [mic, setMic] = useState(false);
+  const [texto,setTexto]=useState("");
+  useEffect(()=>{
+      console.log(transcript)
+      console.log(transcript=="")
+    },[transcript])
+
+  useEffect(()=>{
+    if(mic){
+        SpeechRecognition.startListening({ continuous: true, language: 'es-ES' })
+    }
+    else{
+        SpeechRecognition.stopListening()
+    }
+    },[mic])
+
   return (
     <div className="footer-container">
       <div className="footer">
